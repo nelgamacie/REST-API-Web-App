a9bb9d2a79afac69f929bd61298fe8fb57520132
Nour Mohamed <nmoha8@uwo.ca>
2024-01-07 17:13:43 -0500

    security policiy visibility test

--- my-app/src/app/PrivacyPolicy/page.js
+++ my-app/src/app/PrivacyPolicy/page.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const SecurityPrivacyPolicy = () => {
+  const divStyle = {
+    color: 'white', // Set the text color to white
+  };
+
   return (
-    <div>
+    <div style={divStyle}>
       <h1>Security and Privacy Policy</h1>
       <p>
         Our Security and Privacy Policy outlines how we protect user data and ensure the security of our platform. We prioritize your privacy and take measures to safeguard your information.
